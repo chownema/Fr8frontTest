010ff0f1b3929ba29840fb8dc65b835691392d66
WhatIsOO <chownema@gmail.com>
2020-01-13 11:35:34 +1300

    0.21

--- 0.1/cor/main-es2015.js
+++ 0.1/cor/main-es2015.js
@@ -7481,7 +7481,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: true,
-    backendUrl: 'http://13.211.179.196'
+    backendUrl: 'https://api.innedus.com'
 };
 /*
  * For easier debugging in development mode, you can import the following file
